a87982dbe6e026c13bf4847aa23ad3eaee340801
Stephen Wentling <swentling87@gmail.com>
2017-01-20 23:26:37 -0800

    update to cp2

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -1,10 +1,22 @@
 require "bloc_works/version"
 require "bloc_works/controller"
+require "bloc_works/utility"
+require "bloc_works/router"
+require "bloc_works/dependencies"
 
 module BlocWorks
   class Application
     def call(env)
-      [200, { 'Content-Type'  => 'text/html'}, ["Hello Blocheads!"]]
+      response = self.fav_icon(env)
+
+  		if response
+  			return response
+  		else
+  			cont_array = self.controller_and_action(env)
+  			cont = cont_array.first.new(env)
+  			action_call = cont.send(cont_array.last)
+  			return [200, {'Content-Type' => 'text/html'}, [action_call]]
+  		end
     end
   end
 end
